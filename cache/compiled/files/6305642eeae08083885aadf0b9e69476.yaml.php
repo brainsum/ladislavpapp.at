38ac9ab277494e6f077a427e8e356968d230cd3a
Krisztian Pinter <kpinter@brainsum.com>
2019-06-21 11:46:46 +0200

    LPAP-000 | removed cache.

--- cache/compiled/files/6305642eeae08083885aadf0b9e69476.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/keeteean/Sites/projects/brainsum/ladislavpapp/user/plugins/devtools/devtools.yaml',
-    'modified' => 1561028145,
-    'data' => [
-        'enabled' => true
-    ]
-];
